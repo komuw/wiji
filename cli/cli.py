99ee682d21baf6063d9e2ef5783132bc2b17934c
komuw <komuw05@gmail.com>
2019-03-02 10:05:13 +0300

    g

--- cli/cli.py
+++ cli/cli.py
@@ -69,6 +69,70 @@ async def produce_tasks_continously(task, *args, **kwargs):
         await task.delay(*args, **kwargs)
 
 
+def BLOCKING_DISK_IO(the_broker) -> wiji.task.Task:
+    class BlockingDiskIOTask(wiji.task.Task):
+        async def run(self, *args, **kwargs):
+            print()
+            print("RUNNING BlockingDiskIOTask:")
+            import subprocess
+
+            subprocess.run(["dd", "if=/dev/zero", "of=/dev/null", "bs=500000", "count=1000000"])
+
+    task = BlockingDiskIOTask(the_broker=the_broker, queue_name="BlockingDiskIOTask")
+    return task
+
+
+def BLOCKING_http_task(the_broker) -> wiji.task.Task:
+    class MyTask(wiji.task.Task):
+        async def run(self, *args, **kwargs):
+            print()
+            print("RUNNING BLOCKING_http_task:")
+            import requests
+
+            url = kwargs["url"]
+            resp = requests.get(url)
+            print("resp: ", resp)
+
+    task = MyTask(the_broker=the_broker, queue_name="BlockingHttp_Queue")
+    return task
+
+
+def http_task(the_broker) -> wiji.task.Task:
+    class MyTask(wiji.task.Task):
+        async def run(self, *args, **kwargs):
+            import aiohttp
+
+            url = kwargs["url"]
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    print("resp statsus: ", resp.status)
+                    res_text = await resp.text()
+                    print(res_text[:50])
+
+    task = MyTask(the_broker=the_broker, queue_name="AsyncHttpQueue")
+    return task
+
+
+def print_task(the_broker) -> wiji.task.Task:
+    class MyTask(wiji.task.Task):
+        async def run(self, *args, **kwargs):
+            import hashlib
+
+            print()
+            print("RUNNING print_task:")
+            print("args:", args)
+            print("kwargs:", kwargs)
+            print()
+            h = hashlib.blake2b()
+            h.update(b"Hello world")
+            h.hexdigest()
+            await asyncio.sleep(0.4)
+
+    task = MyTask(the_broker=the_broker, queue_name="PrintQueue")
+    return task
+
+
+################## CHAIN ##################
 def adder_task(the_broker, chain=None) -> wiji.task.Task:
     class AdderTask(wiji.task.Task):
         async def run(self, a, b):
@@ -100,6 +164,36 @@ def divider_task(the_broker, chain=None) -> wiji.task.Task:
     return task
 
 
+def multiplier_task(the_broker, chain=None) -> wiji.task.Task:
+    class MultiplierTask(wiji.task.Task):
+        async def run(self, bbb, a=5.5):
+            res = bbb * a
+            print()
+            print("RUNNING multiplier_task:")
+            print("multiplier: ", res)
+            print()
+            return res
+
+    task = MultiplierTask(the_broker=the_broker, queue_name="MultiplierTaskQueue", chain=chain)
+    return task
+
+
+################## CHAIN ##################
+
+
+def exception_task(the_broker, chain=None) -> wiji.task.Task:
+    class ExceptionTask(wiji.task.Task):
+        async def run(self):
+            print()
+            print("RUNNING exception_task:")
+            print()
+            await asyncio.sleep(0.5)
+            raise ValueError("\n Houston We got 99 problems. \n")
+
+    task = ExceptionTask(the_broker=the_broker, queue_name="ExceptionTaskQueue", chain=chain)
+    return task
+
+
 if __name__ == "__main__":
     main()
     """
@@ -112,17 +206,44 @@ if __name__ == "__main__":
     # 1. publish task
 
     # ##### publish 1 ###############
-    divider = divider_task(the_broker=MY_BROKER)
+    multiplier = multiplier_task(the_broker=MY_BROKER)
+    divider = divider_task(the_broker=MY_BROKER, chain=multiplier)
 
     adder = adder_task(the_broker=MY_BROKER, chain=divider)
 
-    adder.synchronous_delay(3, 7, task_options=wiji.task.TaskOptions(eta=0.01, max_retries=3))
-    # adder.synchronous_delay(a=23, b=67)
+    adder.synchronous_delay(3, 7, task_options=wiji.task.TaskOptions(eta=4.56))
+    #############################################
 
-    all_tasks = [adder, divider]
+    # ALTERNATIVE way of chaining
+    adder = adder_task(the_broker=MY_BROKER)
+    divider = divider_task(the_broker=MY_BROKER)
+    multiplier = multiplier_task(the_broker=MY_BROKER)
+    adder | divider | multiplier
 
-    workers = []  # [wiji.Worker(the_task=wiji.task.WatchDogTask, use_watchdog=True)]
-    producers = []  # [produce_tasks_continously(task=wiji.task.WatchDogTask)]
+    #####################################
+    http_task1 = http_task(the_broker=MY_BROKER)
+    http_task1.synchronous_delay(url="http://httpbin.org/get")
+
+    print_task2 = print_task(the_broker=MY_BROKER)
+    print_task2.synchronous_delay("myarg", my_kwarg="my_kwarg")
+
+    exception_task22 = exception_task(the_broker=MY_BROKER)
+    #####################################
+
+    BLOCKING_task = BLOCKING_http_task(the_broker=MY_BROKER)
+
+    all_tasks = [
+        http_task1,
+        print_task2,
+        adder,
+        divider,
+        multiplier,
+        exception_task22,
+        BLOCKING_task,
+    ]
+
+    workers = [wiji.Worker(the_task=wiji.task.WatchDogTask, use_watchdog=True)]
+    producers = [produce_tasks_continously(task=wiji.task.WatchDogTask)]
 
     for task in all_tasks:
         _worker = wiji.Worker(the_task=task)
@@ -132,7 +253,21 @@ if __name__ == "__main__":
     for i in workers:
         consumers.append(i.consume_forever())
 
-    # producers.extend([produce_tasks_continously(task=adder, a=23, b=67)])
+    producers.extend(
+        [
+            produce_tasks_continously(task=http_task1, url="https://httpbin.org/delay/45"),
+            produce_tasks_continously(task=print_task2, my_KWARGS={"name": "Jay-Z", "age": 4040}),
+            produce_tasks_continously(task=adder, a=23, b=67),
+            produce_tasks_continously(
+                task=exception_task22, task_options=wiji.task.TaskOptions(eta=-34.99)
+            ),
+            produce_tasks_continously(
+                task=BLOCKING_task,
+                url="https://httpbin.org/delay/11",
+                task_options=wiji.task.TaskOptions(eta=2.33),
+            ),
+        ]
+    )
 
     # 2.consume tasks
     async def async_main():

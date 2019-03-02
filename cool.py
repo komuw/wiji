fe67716c917d9db0bd522f98d4f74e96ea7f5b72
komuw <komuw05@gmail.com>
2019-03-02 14:39:52 +0300

    g

--- cool.py
+++ cool.py
@@ -1,72 +1,75 @@
 import asyncio
-import aiohttp
-import requests
-import time
 
 
-async def Async_http():
-    url = "https://httpbin.org/delay/23"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            print()
-            print("Async_http:")
-            print("resp statsus: ", resp.status)
-            res_text = await resp.text()
-            print(res_text[:50])
+class Aiter:
+    def __init__(self, iterable):
+        self.iter_ = iter(iterable)
+
+    async def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        await asyncio.sleep(0)
+        try:
+            object = next(self.iter_)
+        except StopIteration:
+            raise StopAsyncIteration  # :-) PEP492 - "To stop iteration __anext__ must raise a StopAsyncIteration exception"
+
+        return object
 
 
-def SYNC_http():
-    print()
-    print("SYNC_http:")
-    url = "https://httpbin.org/delay/23"
-    resp = requests.get(url)
+class AsyncIterFromTasks:
+    def __init__(self, tasks):
+        self.not_done = list(tasks)
+        self.done = set()
+
+    async def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        if not self.done and self.not_done:
+            self.done, self.not_done = await asyncio.wait(
+                self.not_done, return_when=asyncio.FIRST_COMPLETED
+            )
+        if not self.done:
+            assert not self.not_done
+            raise StopAsyncIteration()
+        return self.done.pop()
 
 
-class MyBase:
-    pass
+class AsyncYield:
+    def __init__(self, value):
+        self.value = value
+
+    def __await__(self):
+        yield self.value
 
 
-class Deriv(MyBase):
-    pass
+class AsyncIter:
+    def __init__(self, ll):
+        self.i = 0
+        self.ll = ll
+        self.len_ = len(self.ll)
+        self.popper = 0
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        self.i += 1
+        if self.i > self.len_:
+            raise StopAsyncIteration
+
+        val = self.ll[self.popper]
+        self.popper += 1
+
+        return await asyncio.sleep(delay=0.05, result=val)
 
 
 if __name__ == "__main__":
 
     async def async_main():
-        xxx = Deriv()
+        async for i in AsyncIter([1, 2, 3]):
+            print(i)
 
-        ############### ASYNC ###############
-        start_time_cpu = time.process_time()
-        start_time_io = time.monotonic()
-
-        await Async_http()
-        end_time_cpu = time.process_time()
-        end_time_io = time.monotonic()
-
-        diff_cpu_time = end_time_cpu - start_time_cpu
-        diff_io_time = end_time_io - start_time_io
-
-        print()
-        print("Async_http took:")
-        print("diff_cpu_time: ", diff_cpu_time)
-        print("diff_io_time: ", diff_io_time)
-        #############################################
-
-        ########### SYNC ###############
-        start_time_cpu = time.process_time()
-        start_time_io = time.monotonic()
-
-        SYNC_http()
-        end_time_cpu = time.process_time()
-        end_time_io = time.monotonic()
-
-        diff_cpu_time = end_time_cpu - start_time_cpu
-        diff_io_time = end_time_io - start_time_io
-
-        print()
-        print("SYNC_http took:")
-        print("diff_cpu_time: ", diff_cpu_time)
-        print("diff_io_time: ", diff_io_time)
-        #############################################
-
-    asyncio.run(async_main(), debug=True)
+    asyncio.run(async_main(), debug=False)

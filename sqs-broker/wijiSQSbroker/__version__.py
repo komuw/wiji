d68b55deeee7dfb868cdbc9d4524aaf22f1f5b47
komuW <komuw05@gmail.com>
2019-03-11 15:29:35 +0300

    ff

--- sqs-broker/wijiSQSbroker/__version__.py
+++
@@ -1,9 +0,0 @@
-about = {
-    "__title__": "wijiSQSbroker",
-    "__description__": "AWS SQS broker for wiji. Wiji<https://github.com/komuw/wiji> is an asyncio distributed task processor/queue.",
-    "__url__": "https://github.com/komuw/wiji/tree/master/sqs-broker",
-    "__version__": "v0.1.0-beta.1",
-    "__author__": "komuW",
-    "__author_email__": "komuw05@gmail.com",
-    "__license__": "MIT",
-}

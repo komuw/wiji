d8cd72f65ec5280e85add7e258cffff774f423dd
komuW <komuw05@gmail.com>
2019-03-11 15:15:23 +0300

    ff

--- sqs-broker/wijiSQSbroker/broker.py
+++ sqs-broker/wijiSQSbroker/broker.py
@@ -1,11 +1,20 @@
 import wiji
 import typing
+import asyncio
 import logging
+import datetime
+import functools
+import concurrent
 import botocore.session
 
 
 # See SQS limits: https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-limits.html
 # TODO: we need to add this limits as validations to this broker
+
+
+# TODO: add batching
+# TODO: add compression
+# TODO: add long-polling; https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-long-polling.html
 
 
 class SqsBroker(wiji.broker.BaseBroker):
@@ -21,6 +30,7 @@ class SqsBroker(wiji.broker.BaseBroker):
         MaximumMessageSize: int = 262_144,
         ReceiveMessageWaitTimeSeconds: int = 0,
         VisibilityTimeout: int = 30,
+        DelaySeconds: int = 0,
         loglevel: str = "DEBUG",
         log_handler: typing.Union[None, wiji.logger.BaseLogger] = None,
     ) -> None:
@@ -32,6 +42,7 @@ class SqsBroker(wiji.broker.BaseBroker):
             MaximumMessageSize=MaximumMessageSize,
             ReceiveMessageWaitTimeSeconds=ReceiveMessageWaitTimeSeconds,
             VisibilityTimeout=VisibilityTimeout,
+            DelaySeconds=DelaySeconds,
             loglevel=loglevel,
             log_handler=log_handler,
         )
@@ -47,7 +58,9 @@ class SqsBroker(wiji.broker.BaseBroker):
         self.MaximumMessageSize = MaximumMessageSize
         self.ReceiveMessageWaitTimeSeconds = ReceiveMessageWaitTimeSeconds
         self.VisibilityTimeout = VisibilityTimeout
+        self.DelaySeconds = DelaySeconds
 
+        self.QueueUrl: typing.Union[None, str] = None
         self.region_name = region_name
         self.aws_access_key_id = aws_access_key_id
         self.aws_secret_access_key = aws_secret_access_key
@@ -61,6 +74,10 @@ class SqsBroker(wiji.broker.BaseBroker):
             use_ssl=True,
         )
 
+        self.task_receipt: str = {}
+        self._thread_name_prefix: str = "wiji-SqsBroker-thread-pool"
+        self.loop: asyncio.events.AbstractEventLoop = asyncio.get_event_loop()
+
     def _validate_task_args(
         self,
         region_name: str,
@@ -70,6 +87,7 @@ class SqsBroker(wiji.broker.BaseBroker):
         MaximumMessageSize: int,
         ReceiveMessageWaitTimeSeconds: int,
         VisibilityTimeout: int,
+        DelaySeconds: int,
         loglevel: str,
         log_handler: typing.Union[None, wiji.logger.BaseLogger],
     ) -> None:
@@ -109,6 +127,7 @@ class SqsBroker(wiji.broker.BaseBroker):
             MaximumMessageSize=MaximumMessageSize,
             ReceiveMessageWaitTimeSeconds=ReceiveMessageWaitTimeSeconds,
             VisibilityTimeout=VisibilityTimeout,
+            DelaySeconds=DelaySeconds,
         )
 
     def _validate_sqs(
@@ -117,6 +136,7 @@ class SqsBroker(wiji.broker.BaseBroker):
         MaximumMessageSize: int,
         ReceiveMessageWaitTimeSeconds: int,
         VisibilityTimeout: int,
+        DelaySeconds: int,
     ):
         if not isinstance(MessageRetentionPeriod, int):
             raise ValueError(
@@ -168,6 +188,17 @@ class SqsBroker(wiji.broker.BaseBroker):
         elif VisibilityTimeout > 43200:
             raise ValueError("""`VisibilityTimeout` should not be greater than 43200 seconds""")
 
+        if not isinstance(DelaySeconds, int):
+            raise ValueError(
+                """`DelaySeconds` should be of type:: `int` You entered: {0}""".format(
+                    type(DelaySeconds)
+                )
+            )
+        if DelaySeconds < 0:
+            raise ValueError("""`DelaySeconds` should not be less than 0 seconds""")
+        elif DelaySeconds > 900:
+            raise ValueError("""`DelaySeconds` should not be greater than 900 seconds""")
+
     def _sanity_check_logger(self, event: str) -> None:
         """
         Called when we want to make sure the supplied logger can log.
@@ -186,6 +217,14 @@ class SqsBroker(wiji.broker.BaseBroker):
         - A queue name can have up to 80 characters.
           Valid values: alphanumeric characters, hyphens (- ), and underscores (_ ).
         """
+        with concurrent.futures.ThreadPoolExecutor(
+            thread_name_prefix=self._thread_name_prefix
+        ) as executor:
+            await self.loop.run_in_executor(
+                executor, functools.partial(self.blocking_check, queue_name=queue_name)
+            )
+
+    def blocking_check(self, queue_name: str) -> None:
         try:
             response = self.client.create_queue(
                 QueueName=queue_name,
@@ -194,8 +233,11 @@ class SqsBroker(wiji.broker.BaseBroker):
                     "MaximumMessageSize": self.MaximumMessageSize,
                     "ReceiveMessageWaitTimeSeconds": self.ReceiveMessageWaitTimeSeconds,
                     "VisibilityTimeout": self.VisibilityTimeout,
+                    "DelaySeconds": self.DelaySeconds,
                 },
             )
+            QueueUrl = response["QueueUrl"]
+            self.QueueUrl = QueueUrl
         except Exception as e:
             raise e
 
@@ -203,23 +245,91 @@ class SqsBroker(wiji.broker.BaseBroker):
         self, item: str, queue_name: str, task_options: wiji.task.TaskOptions
     ) -> None:
         """
-        enqueue/save an item.
-
-        Parameters:
-            item: The item to be enqueued/saved
-            queue_name: name of queue to enqueue in
-            task_options: options for the specific task been enqueued
         """
-        raise NotImplementedError("`enqueue` method must be implemented.")
+        with concurrent.futures.ThreadPoolExecutor(
+            thread_name_prefix=self._thread_name_prefix
+        ) as executor:
+            await self.loop.run_in_executor(
+                executor,
+                functools.partial(
+                    self.blocking_enqueue,
+                    item=item,
+                    queue_name=queue_name,
+                    task_options=task_options,
+                ),
+            )
+
+    def blocking_enqueue(
+        self, item: str, queue_name: str, task_options: wiji.task.TaskOptions
+    ) -> None:
+        now = datetime.datetime.now(tz=datetime.timezone.utc)
+        task_eta = task_options.eta
+        task_eta = wiji.protocol.Protocol._from_isoformat(task_eta)
+
+        delay = self.DelaySeconds
+        if task_eta > now:
+            diff = task_eta - now
+            delay = diff.seconds
+            if delay > 900:
+                delay = 900
+
+        response = self.client.send_message(
+            QueueUrl=self.QueueUrl,
+            MessageBody=item,
+            DelaySeconds=delay,
+            MessageAttributes={
+                "string": {
+                    "user": "wiji.SqsBroker",
+                    "task_eta": task_options.eta,
+                    "task_id": task_options.task_id,
+                    "task_hook_metadata": task_options.hook_metadata,
+                }
+            },
+        )
+        _ = response["MD5OfMessageBody"]
+        _ = response["MD5OfMessageAttributes"]
+        _ = response["MessageId"]
+        _ = response["SequenceNumber"]
 
     async def dequeue(self, queue_name: str) -> str:
         """
-        dequeue an item.
-
-        Returns:
-            item that was dequeued
         """
-        raise NotImplementedError("`dequeue` method must be implemented.")
+        with concurrent.futures.ThreadPoolExecutor(
+            thread_name_prefix=self._thread_name_prefix
+        ) as executor:
+            while True:
+                item = await self.loop.run_in_executor(
+                    executor, functools.partial(self.blocking_dequeue, queue_name=queue_name)
+                )
+                if item:
+                    return item
+                else:
+                    await asyncio.sleep(5)
+
+    def blocking_dequeue(self, queue_name: str) -> str:
+        response = self.client.receive_message(
+            QueueUrl=self.QueueUrl,
+            AttributeNames=["All"],
+            MessageAttributeNames=["All"],
+            MaxNumberOfMessages=1,
+            # VisibilityTimeout: The duration (in seconds) that the received messages are hidden
+            # from subsequent retrieve requests after being retrieved by a ReceiveMessage request.
+            VisibilityTimeout=self.VisibilityTimeout,
+            # WaitTimeSeconds: The duration (in seconds) for which the call waits for a message to arrive in the queue before returning.
+            # If no messages are available and the wait time expires, the call returns successfully with an empty list of messages.
+            WaitTimeSeconds=5,
+        )
+
+        if len(response["Messages"]) >= 1:
+            ReceiptHandle = response["Messages"][0]["ReceiptHandle"]
+            MessageAttributes = response["Messages"][0]["MessageAttributes"]
+            task_id = MessageAttributes["task_id"]
+            self.task_receipt[task_id] = ReceiptHandle
+
+            item = response["Messages"][0]["Body"]
+            return item
+        else:
+            return None
 
     async def done(
         self,
@@ -229,7 +339,30 @@ class SqsBroker(wiji.broker.BaseBroker):
         state: wiji.task.TaskState,
     ) -> None:
         """
-        called by wiji worker once it is done executing a task.
-        the broker can then decide to do any clean up actions like removing that task from the queue etc.
         """
-        raise NotImplementedError("`done` method must be implemented.")
+        with concurrent.futures.ThreadPoolExecutor(
+            thread_name_prefix=self._thread_name_prefix
+        ) as executor:
+            await self.loop.run_in_executor(
+                executor,
+                functools.partial(
+                    self.blocking_done,
+                    item=item,
+                    queue_name=queue_name,
+                    task_options=task_options,
+                    state=state,
+                ),
+            )
+
+    def blocking_done(
+        self,
+        item: str,
+        queue_name: str,
+        task_options: wiji.task.TaskOptions,
+        state: wiji.task.TaskState,
+    ) -> None:
+        ReceiptHandle = self.task_receipt.pop(task_options.task_id, None)
+        if ReceiptHandle:
+            response = self.client.delete_message(
+                QueueUrl=self.QueueUrl, ReceiptHandle=ReceiptHandle
+            )

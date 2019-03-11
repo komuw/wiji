e545ea18689d7d08b7528c4e0c2afab52b6ae7ae
komuW <komuw05@gmail.com>
2019-03-11 12:27:03 +0300

    ff

--- sqs-broker/setup.py
+++ sqs-broker/setup.py
@@ -81,7 +81,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["boto3==1.9.111"],
+    install_requires=["botocore==1.12.111"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:

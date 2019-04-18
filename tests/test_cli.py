ea1e65813b0d41d6dd3848b4a3223b56c0da5b93
komuW <komuw05@gmail.com>
2019-04-18 16:47:13 +0300

    f

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,24 @@
+import uuid
+import signal
+import asyncio
 import argparse
 from unittest import TestCase, mock
 
 import cli
+import wiji
+
+
+def AsyncMock(*args, **kwargs):
+    """
+    see: https://blog.miguelgrinberg.com/post/unit-testing-asyncio-code
+    """
+    m = mock.MagicMock(*args, **kwargs)
+
+    async def mock_coro(*args, **kwargs):
+        return m(*args, **kwargs)
+
+    mock_coro.mock = m
+    return mock_coro
 
 
 class MockArgumentParser:
@@ -39,3 +56,42 @@ class TestCli(TestCase):
         with mock.patch("argparse.ArgumentParser") as mock_ArgumentParser:
             mock_ArgumentParser.return_value = MockArgumentParser(wiji_config=self.wiji_config)
             cli.cli.main()
+
+
+class TestCliSigHandling(TestCase):
+    """
+    run tests as:
+        python -m unittest discover -v -s .
+    run one testcase as:
+        python -m unittest -v tests.test_cli.TestCliSigHandling.test_something
+    """
+
+    def setUp(self):
+        class AdderTask(wiji.task.Task):
+            the_broker = wiji.broker.InMemoryBroker()
+            queue_name = "{0}-AdderTaskQueue".format(uuid.uuid4())
+            drain_duration = 0.01
+
+            async def run(self, a, b):
+                return a + b
+
+        self.workers = [wiji.Worker(the_task=AdderTask(), worker_id="myWorkerID1")]
+        self.logger = wiji.logger.SimpleLogger("wiji.TestCliSigHandling")
+
+    def tearDown(self):
+        pass
+
+    @staticmethod
+    def _run(coro):
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(coro)
+
+    def test_success_signal_handling(self):
+        self._run(cli.utils.sig._signal_handling(logger=self.logger, workers=self.workers))
+
+    def test_success_handle_termination_signal(self):
+        self._run(
+            cli.utils.sig._handle_termination_signal(
+                logger=self.logger, _signal=signal.SIGTERM, workers=self.workers
+            )
+        )

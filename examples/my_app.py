be29499bb0aaf173073bc9e7aa0dc423a6b80481
komuW <komuw05@gmail.com>
2019-05-23 14:26:14 +0300

    f

--- examples/my_app.py
+++ examples/my_app.py
@@ -1,16 +1,8 @@
 import wiji
 
 
-# BROKER = wiji.broker.InMemoryBroker()
-
-from examples.redis_broker import ExampleRedisBroker
-
-
-MY_BROKER = ExampleRedisBroker()
-
-
 class AdderTask(wiji.task.Task):
-    the_broker = MY_BROKER
+    the_broker = wiji.broker.InMemoryBroker()
     queue_name = "AdderTaskQueue1"
 
     async def run(self, a, b):
@@ -23,10 +15,9 @@ class AdderTask(wiji.task.Task):
 
 # run cli as:
 #   wiji-cli --app examples.my_app.MyAppInstance
-MyAppInstance = wiji.app.App(task_classes=[AdderTask], watchdog_duration=20.0)
+MyAppInstance = wiji.app.App(task_classes=[AdderTask])
 
 
 if __name__ == "__main__":
-    for i in range(0, 122):
-        myAdderTask = AdderTask()
-        myAdderTask.synchronous_delay(67, i)
+    myAdderTask = AdderTask()
+    myAdderTask.synchronous_delay(67, 887)
